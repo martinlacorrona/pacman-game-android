9c794f13c643ddfccc3174419d495b3513b2e60f
Martin Fernandez Prieto <tatin1998@gmail.com>
2019-11-27 13:51:32 +0100

    0.2: pantalla de cargando

--- app/src/main/java/com/github/martinlacorrona/pacmangame/MainActivity.java
+++ app/src/main/java/com/github/martinlacorrona/pacmangame/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     private Context context;
+    private WebView wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +21,27 @@ public class MainActivity extends Activity {
 
         context = this;
 
-        WebView wv = new WebView(this);
-        wv.getSettings().setJavaScriptEnabled(true);
-        wv.loadUrl("https://martinlacorrona.github.io/pacman-game/");
-        wv.setWebViewClient(new WebViewClient() {
-
-            public void onPageFinished(WebView view, String url) {
-                Toast.makeText(context, "Pagina cargada", Toast.LENGTH_LONG).show();
-            }
-        });
-
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE;
         decorView.setSystemUiVisibility(uiOptions);
 
+        wv = new WebView(this);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("https://martinlacorrona.github.io/pacman-game/");
+        wv.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                showGame();
+            }
+        });
+
+        setContentView(R.layout.loading);
+    }
+
+    private void showGame() {
+        wv.setVisibility(View.INVISIBLE);
         setContentView(wv);
+        wv.setVisibility(View.VISIBLE);
     }
 }
